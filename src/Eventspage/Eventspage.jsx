4a0a48489e1da9543d1d3b177975fa4a6018efa3
Vaishakh Nambiar <vaishakhnambiar2002@gmail.com>
2023-04-15 00:11:58 +0530

    Landing done

---
+++ src/Eventspage/Eventspage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Eventspage.css";
+
+export default function Eventspage() {
+  return (
+    <section className="eventspage ot-red">
+      <div className="event-title-box ">
+        <div className="ff-tc event-title title">Events</div>
+      </div>
+      <div className="event-list ot-blue"></div>
+    </section>
+  );
+}
---
+++ src/intro/Introloader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Introloader() {
+  return (
+    <section className="intro-1 ff-tc ">
+      <div className="intro-box">
+        <div className="intro-btn"></div>
+        <div className="intro-btn-txt">GG</div>
+      </div>
+    </section>
+  );
+}
